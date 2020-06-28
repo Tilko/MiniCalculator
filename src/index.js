76c1b24b1a96b496ce2ce900a60a78607f8f44eb
Florian Kempenich <Flori@nKempenich.com>
2020-06-28 18:50:12 +0100

    Minor refactor

--- src/index.js
+++ src/index.js
@@ -6,11 +6,16 @@ const initCalculator = () => {
   const screen = document.querySelector(".screen");
   const allDigits = document.querySelectorAll(".digit");
 
+  const updateDisplay = (toDisplay) => {
+    screen.textContent = toDisplay;
+  };
+
   const sendToCalculatorUIAndUpdateDisplay = (event) => {
     const clickedDigitDiv = event.target;
-    const digitAsText = clickedDigitDiv.textContent;
-    calculatorUI.digitClicked(Number(digitAsText));
-    screen.textContent = calculatorUI.numberDisplayed;
+    const digit = Number(clickedDigitDiv.textContent);
+
+    calculatorUI.digitClicked(digit);
+    updateDisplay(calculatorUI.numberDisplayed);
   };
 
   allDigits.forEach((numberDiv) =>

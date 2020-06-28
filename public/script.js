905fc4a1cb6e1df4519573b447d95a3680dcaff0
Florian Kempenich <Flori@nKempenich.com>
2020-06-28 15:53:38 +0100

    PASS: Add test digit show on screen

--- public/script.js
+++ public/script.js
@@ -1,11 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
+const sandbox = () => {
+  const btn = document.querySelector("#btn");
+  const debugDiv = document.querySelector(".debug");
 
+  btn.addEventListener("click", () => {
+    debugDiv.classList.remove("hidden");
+  });
+};
+const initCalculator = () => {
 
-  const btn = document.querySelector('#btn');
-  const debugDiv = document.querySelector('.debug')
+  const screen = document.querySelector('.screen')
+  const allDigits = document.querySelectorAll('.number')
 
-  btn.addEventListener('click', () => {
-    debugDiv.classList.remove('hidden')
+  allDigits.forEach(numberDiv => {
+    numberDiv.addEventListener('click', () => {
+      const number = numberDiv.textContent;
+      screen.innerText = number;
+    })
   })
-
-})
+}
+document.addEventListener("DOMContentLoaded", initCalculator);
+document.addEventListener("DOMContentLoaded", sandbox);

cc33ccd7fb7470bc850c186b0f8a911944ea479d
Florian Kempenich <Flori@nKempenich.com>
2020-06-28 17:56:42 +0100

    Install and setup Webpack

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    compress: true,
+    port: 1234
+  }
+};
+
+module.exports = config;

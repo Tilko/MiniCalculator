13414955c335a51744a1940c7baa74305c27c906
Florian Kempenich <Flori@nKempenich.com>
2020-06-28 16:11:38 +0100

    Refactor

--- public/script.js
+++ public/script.js
@@ -7,16 +7,19 @@ const sandbox = () => {
   });
 };
 const initCalculator = () => {
+  const screen = document.querySelector(".screen");
+  const allDigits = document.querySelectorAll(".digit");
 
-  const screen = document.querySelector('.screen')
-  const allDigits = document.querySelectorAll('.digit')
+  const displayClickedDigit = (event) => {
+    const clickedDigitDiv = event.target;
+    const digit = clickedDigitDiv.textContent;
+    screen.innerText = digit;
+  };
 
-  allDigits.forEach(numberDiv => {
-    numberDiv.addEventListener('click', () => {
-      const number = numberDiv.textContent;
-      screen.innerText = number;
-    })
-  })
-}
+  allDigits.forEach((numberDiv) =>
+    numberDiv.addEventListener("click", displayClickedDigit)
+  );
+
+};
 document.addEventListener("DOMContentLoaded", initCalculator);
 document.addEventListener("DOMContentLoaded", sandbox);

1537a3d155d32e80ca93108e6f948f4ac381e061
Florian Kempenich <Flori@nKempenich.com>
2020-06-28 21:10:22 +0100

    Fix calculator design

--- src/calculator.js
+++ src/calculator.js
@@ -1,9 +1,18 @@
 class Calculator {
   constructor() {
     this.result = 0;
+    this.currentOp;
   }
   add(number) {
     this.result += number;
+    this.currentOp = "add";
+  }
+  equal(number) {
+    if (this.currentOp === "add") {
+      this.result += number;
+    } else{
+      throw 'Not implemented'
+    }
   }
 }
 
--- src/calculator.test.js
+++ src/calculator.test.js
@@ -8,7 +8,7 @@ describe("Calculator", () => {
 
   it('can add 2 numbers', () => {
     calculator.add(1234)
-    calculator.add(5678)
+    calculator.equal(5678)
     expect(calculator.result).toBe(1234 + 5678)
   })
 

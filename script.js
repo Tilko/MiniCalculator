47aaaf34b4b251888b0adac29e4433d3f8114b35
Florian Kempenich <Flori@nKempenich.com>
2020-06-28 13:49:36 +0100

    Add basic demo behavior

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('hello')
+document.addEventListener('DOMContentLoaded', () => {
+
+
+  const btn = document.querySelector('#btn');
+  const debugDiv = document.querySelector('.debug')
+
+  btn.addEventListener('click', () => {
+    debugDiv.classList.remove('hidden')
+  })
+
+})

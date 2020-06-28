54639c9fa2dc1d07085636b7330102572fd9b192
Florian Kempenich <Flori@nKempenich.com>
2020-06-28 15:23:36 +0100

    Calculator display numbers

--- cypress/integration/miniCalculator.spec.js
+++ cypress/integration/miniCalculator.spec.js
@@ -12,3 +12,18 @@ describe("Debug Button", () => {
     cy.get('.debug').should('be.not.visible')
   });
 });
+
+describe("Calculator", () => {
+  it("should display numbers", () => {
+    cy.contains('0').should('be.visible')
+    cy.contains('1').should('be.visible')
+    cy.contains('2').should('be.visible')
+    cy.contains('3').should('be.visible')
+    cy.contains('4').should('be.visible')
+    cy.contains('5').should('be.visible')
+    cy.contains('6').should('be.visible')
+    cy.contains('7').should('be.visible')
+    cy.contains('8').should('be.visible')
+    cy.contains('9').should('be.visible')
+  })
+})

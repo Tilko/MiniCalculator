624756f0f9fc9fef4173bfe8a7808f826d646414
gmart <martinetti.gregoire@gmail.com>
2020-07-01 11:49:30 +0200

    Fix the operators order => substraction pass

--- src/Operators_Id_To_Code.js
+++ src/Operators_Id_To_Code.js
@@ -1,7 +1,7 @@
 module.exports = {
     plus: (lhs, rhs) => lhs + rhs,
-    minus: (lhs, rhs) => lhs + rhs,
-    mult: (lhs, rhs) => lhs - rhs,
-    div: (lhs, rhs) => lhs * rhs,
-    equal: (lhs, rhs) => lhs / rhs
+    minus: (lhs, rhs) => lhs - rhs,
+    mult: (lhs, rhs) => lhs * rhs,
+    div: (lhs, rhs) => lhs / rhs,
+    equal: (lhs, rhs) => {}
   };

66f90a3645b2579e0d3ec0493baabfba1c1ed0ee
gmart <martinetti.gregoire@gmail.com>
2020-07-02 20:19:40 +0200

    amend prev (forgot that index.js)

--- src/index.js
+++ src/index.js
@@ -2,47 +2,63 @@ const sandbox = require("./sandbox");
 const CalculatorUI = require("./calculatorUI");
 const Calculator = require('./calculator')
 const operators_Id_To_Code = require("./Operators_Id_To_Code")
+const TextAndScreenMatcher = require("./lib/textAndScreenMatching.js");
 const calculator = new Calculator()
 const calculatorUI = new CalculatorUI();
 
 const initCalculator = () => {
 
-  const screen = document.querySelector(".screen");
-  const clearButton = document.querySelector('#clear')
-  const signToogleButton = document.querySelector('#signToggle')
-  const dotButton = document.querySelector('#dot')
+    const screen = document.querySelector(".screen");
+    const clearButton = document.querySelector('#clear')
+    const signToogleButton = document.querySelector('#signToggle')
+    const dotButton = document.querySelector('#dot')
 
-  const updateScreen = toDisplay => {
-    screen.textContent = toDisplay;
-  };
+    const updateScreen = toDisplay => {
+        let numberToDisplay = "" + toDisplay;
 
-  const allDigitDivs = document.querySelectorAll(".digit");
-  allDigitDivs.forEach(digitDiv => digitDiv.addEventListener("click", event => {
-    calculatorUI.appendDigit(event.target.textContent);
-    updateScreen(calculatorUI.getValueString());
-  }));
+        const calcWidth = document.getElementsByClassName("calculator")[0]
+            .getBoundingClientRect().width;
+        const adapter = new TextAndScreenMatcher(15, calcWidth - 20, 'font-family: "Work Sans", sans-serif;');
+        let adaptedFont;
+        ({ text: numberToDisplay, adaptedFont } = adapter.adaptTextAndFont(numberToDisplay, 55));
+        screen.style.fontSize = adaptedFont + "px";
+        screen.style.cssText = `
+          display: flex;
+          justify-content: flex-end;
+          align-items: center;
+          font-size: ${adaptedFont}px;
+          `
 
-  for (const opId of Object.keys(operators_Id_To_Code)) {
-    document.querySelector('#' + opId).addEventListener('click', () => {
-      const rez = calculator.treatOperation(Number(calculatorUI.getValueString()), opId);
-      updateScreen(rez);
-      calculatorUI.setValue(opId == 'equal' ? rez : 0);
+        screen.textContent = numberToDisplay;
+    };
+
+    const allDigitDivs = document.querySelectorAll(".digit");
+    allDigitDivs.forEach(digitDiv => digitDiv.addEventListener("click", event => {
+        calculatorUI.appendDigit(event.target.textContent);
+        updateScreen(calculatorUI.getValueString());
+    }));
+
+    for (const opId of Object.keys(operators_Id_To_Code)) {
+        document.querySelector('#' + opId).addEventListener('click', () => {
+            const rez = calculator.treatOperation(Number(calculatorUI.getValueString()), opId);
+            updateScreen(rez);
+            calculatorUI.setValue(opId == 'equal' ? rez : 0);
+        })
+    }
+
+    clearButton.addEventListener('click', () => {
+        calculatorUI.resetValue();
+        updateScreen(0);
+        calculator.reset();
     })
-  }
-
-  clearButton.addEventListener('click', () => {
-    calculatorUI.resetValue();
-    updateScreen(0);
-    calculator.reset();
-  })
-  signToogleButton.addEventListener('click', () => {
-    calculatorUI.toggleSignum();
-    updateScreen(calculatorUI.getValueString());
-  })
-  dotButton.addEventListener('click', () => {
-    calculatorUI.setDecimalDot();
-    updateScreen(calculatorUI.getValueString());
-  })
+    signToogleButton.addEventListener('click', () => {
+        calculatorUI.toggleSignum();
+        updateScreen(calculatorUI.getValueString());
+    })
+    dotButton.addEventListener('click', () => {
+        calculatorUI.setDecimalDot();
+        updateScreen(calculatorUI.getValueString());
+    })
 };
 document.addEventListener("DOMContentLoaded", initCalculator);
 document.addEventListener("DOMContentLoaded", sandbox.run);
